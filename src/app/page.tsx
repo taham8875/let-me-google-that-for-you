1d34e25bd33927cfb85ca50cf3fe2e324129f7e4
taham8875 <taha.m8875@gmail.com>
2024-02-06 20:30:29 +0200

    Feat: Add Footer component and update page layout
    - Introduce new `src/components/footer.tsx` component to encapsulate footer functionality
    - Modify `src/app/page.tsx` to include Footer component in the page layout
    - Update styles in `src/app/globals.css` to improve layout and responsiveness
    
    This commit enhances the project structure by introducing a separate Footer component for better code organization and updates the page layout to include the Footer component. Additionally, it improves styling to enhance layout and responsiveness.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function page() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Command } from "lucide-react";
+import Link from "next/link";
+
+const linkClasses = "font-semibold underline";
+
+export default function Footer() {
+  return (
+    <footer className="min-w-full text-sm sm:flex sm:flex-row sm:items-center gap-2 p-6 flex flex-col text-center items-center absolute bottom-0">
+      <Command />
+      <p className="max-sm:max-w-80 ">
+        Build by{" "}
+        <Link className={linkClasses} href="https://github.com/taham8875">
+          Taha
+        </Link>
+        . Hosted on{" "}
+        <Link className={linkClasses} href="https://vercel.com">
+          Vercel
+        </Link>
+        . The source code is available on{" "}
+        <Link
+          className={linkClasses}
+          href="https://github.com/taham8875/let-me-google-that-for-you"
+        >
+          GitHub
+        </Link>
+        .
+      </p>
+    </footer>
+  );
+}
